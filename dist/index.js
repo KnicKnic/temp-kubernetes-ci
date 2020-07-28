03b145d262759135f89552654f707784138ecc93
Knic Knic <knic.knic@gmail.com>
2020-07-28 13:51:47 -0700

    reorder sleep, to see node is fully up

--- dist/index.js
+++ dist/index.js
@@ -1633,10 +1633,11 @@ sudo chmod 777 ~/.kube/config
 # systemctl status k3s
 # sleep 15
 cat ~/.kube/config
-kubectl get node
 
 # this is to ensure that everything settled (there are cases where default service accounts were not yet created)
 sleep 4
+
+kubectl get node
 `
 
 let linuxShell = 'bash'
--- linux.sh
+++ linux.sh
@@ -7,7 +7,8 @@ sudo chmod 777 ~/.kube/config
 # systemctl status k3s
 # sleep 15
 cat ~/.kube/config
-kubectl get node
 
 # this is to ensure that everything settled (there are cases where default service accounts were not yet created)
 sleep 4
+
+kubectl get node
